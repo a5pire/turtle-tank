6c4406115c9865b6372012e450fe6892dc41d332
Tim <tim.lawrence@westnet.com.au>
2020-06-22 11:24:32 +1000

    amended database table names

--- turtletank/aquarium.py
+++ turtletank/aquarium.py
@@ -20,6 +20,7 @@ def index():
     return render_template('aquarium/index.html', tanks=tanks)
 
 
+# creates a tank
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
@@ -48,3 +49,61 @@ def create():
             return redirect(url_for('aquarium.index'))
 
     return render_template('aquarium/create.html')
+
+
+def get_tank(id, check_aquarist=True):
+    tank = get_db().execute(
+        'SELECT t.id, name, length, width, depth, volume, tank_owner'
+        ' FROM tank t JOIN aquarist a ON t.tank_owner = a.id'
+        ' WHERE t.id = ?',
+        (id,)
+    ).fetchone()
+
+    if tank is None:
+        abort(404, "Tank id {0} doesn't exist.".format(id))
+
+    if check_aquarist and tank['tank_owner'] != g.user['id']:
+        abort(403)
+
+    return tank
+
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    tank = get_tank(id)
+
+    if request.method == 'POST':
+        name = request.form['name']
+        length = request.form['length']
+        width = request.form['width']
+        depth = request.form['depth']
+        volume = round((length * width) * depth / 1000000, 2)
+        error = None
+
+        if not name:
+            error = 'Name is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE tank SET name = ?, length = ?, width = ?, depth = ?, volume = ?'
+                ' WHERE id = ?',
+                (name, length, width, depth, volume, id)
+            )
+            db.commit()
+            return redirect(url_for('aquarium.index'))
+
+    return render_template('aquarium/update.html', post=tank)
+
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_tank(id)
+    db = get_db()
+    db.execute('DELETE FROM tank WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('aquarium.index'))
